fdf3671f503d887e2d54019f6e8993a07b48dabe
Uroš Polanc <up4473@gmail.com>
2014-12-13 20:58:53 +0000

    Commit #149

--- app.js
+++ app.js
@@ -196,3 +196,12 @@ function displayInfo (ehrId){
 		}
 	});
 }
+
+$(".meter > span").each(function() {
+	$(this)
+		.data("origWidth", $(this).width())
+		.width(0)
+		.animate({
+			width: $(this).data("origWidth")
+		}, 1200);
+});
