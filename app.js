e52863e75c25a88e67cd9aa0ce0b1f8596c7a074
Uroš Polanc <up4473@gmail.com>
2014-12-21 12:50:15 +0000

    Commit #325

--- app.js
+++ app.js
@@ -256,7 +256,7 @@ function displayInfo () {
 			$("#doc_loca").append(location);
 
 			displayLocation ();
-			pieGraph();
+//			displayGraphs();
 		},
 		error: function(err) {
 			return;
@@ -364,6 +364,7 @@ function clean_graph () {
 	element = document.getElementById("graph");
 	element.innerHTML = '';
 }
+
 /*
 function displayGraphs () {
 	clean_graph();
@@ -448,6 +449,7 @@ function type (d) {
 	return d;
 }
 */
+
 function channelVideo () {
 
 	$.support.cors= true;
@@ -479,52 +481,5 @@ function channelVideo () {
 			var text = "<object>" + "<param name='movie' value='" + url + "'></param>" + "<param name='allowFullScreen' value='true'></param>" + "<embed src='" + url + "' type='application/x-shockwave-flash' allowfullscreen='true'></embed>" + "</object>";
 			$("#video").append(text);
 		});
-	});
-}
-
-function pieGraph () {
-	clean_graph();
-	var width = 960,
-	    height = 500,
-	    radius = Math.min(width, height) / 2;
-	
-	var color = d3.scale.ordinal()
-	    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b"]);
-	
-	var arc = d3.svg.arc()
-	    .outerRadius(radius - 10)
-	    .innerRadius(0);
-	
-	var pie = d3.layout.pie()
-	    .sort(null)
-	    .value(function(d) { return d.population; });
-	
-	var svg = d3.select("#graph").append("svg")
-	    .attr("width", width)
-	    .attr("height", height)
-	  .append("g")
-	    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-	
-	var datas = [{"type": "Underweight", "value": 10},{"type": "Normal", "value": 45},{"type": "Overweight", "value": 30},{"type": "Obese", "value": 15}];
-	d3.csv(datas, function(error, data) {
-	
-	  data.forEach(function(d) {
-	    d.value = +d.value;
-	  });
-	
-	  var g = svg.selectAll(".arc")
-	      .data(pie(data))
-	    .enter().append("g")
-	      .attr("class", "arc");
-	
-	  g.append("path")
-	      .attr("d", arc)
-	      .style("fill", function(d) { return color(d.data.type); });
-	
-	  g.append("text")
-	      .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
-	      .attr("dy", ".35em")
-	      .style("text-anchor", "middle")
-	      .text(function(d) { return d.data.type; });
 	});
 }
